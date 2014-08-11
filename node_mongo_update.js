29484b06b059c156345bab0bdf18a5a564cb31a9
Baargav Venkatesh <kon7341@ex.kroger.com>
2014-08-10 23:36:26 -0400

    DCP_GIST: Added mongo ranking Scheduler(Skeletal)

---
+++ node_mongo_update.js
@@ -0,0 +1,21 @@
+/*
+run as a seperate node process. 
+	$ node node_mongo_update
+This is run hourly to update the scores of each article
+Should this be run hourly?
+*/
+var mongo = require('mongodb');
+var monk = require('monk');
+
+
+var minutes = 1, the_interval = minutes * 60 * 1000;
+setInterval(function() {
+    var db = monk('localhost:27017/nodetest1');
+    var collection = db.get('usercollection');
+    collection.find({type:"link"},{sort:[["score","desc"]]},function(e,docs){
+	console.log(docs);
+    });
+  console.log("Completed Updating mongo with new scores");
+}, the_interval);
+
+
