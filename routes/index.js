92be8568fafd4350bf269a6b3f53dd1466a2bf13
Haritha Kalyanaraman <kon6034@ex.kroger.com>
2014-08-25 23:57:35 -0400

    Sigh. Typo

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 router.get('/articles', function(req, res){
 	article.find({}, function(err, articles){
         res.setHeader('Content-Type', 'application/json');
-        res.render('index',{array: docs });
+        res.render('index',{array: articles});
    });
 });
 
