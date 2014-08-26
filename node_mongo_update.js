1f4945addc18054eb32608f89fde2478880f360f
Haritha Kalyanaraman <kon6034@ex.kroger.com>
2014-08-25 23:39:41 -0400

    Fixing monk require error- again

--- node_mongo_update.js
+++ node_mongo_update.js
@@ -4,15 +4,13 @@ This is run hourly to update the scores of each article
 Should this be run hourly?
 */
 var mongo = require('mongodb');
-var monk = require('monk');
-
 
 var minutes = 1, the_interval = minutes * 60 * 1000;
 setInterval(function() {
-    var db = monk('localhost:27017/nodetest1');
-    var collection = db.get('usercollection');
-    collection.find({type:"link"},{sort:[["score","desc"]]},function(e,docs){
-	console.log(docs);
+ //    var db = monk('localhost:27017/nodetest1');
+ //    var collection = db.get('usercollection');
+ //    collection.find({type:"link"},{sort:[["score","desc"]]},function(e,docs){
+	// console.log(docs);
     });
   console.log("Completed Updating mongo with new scores");
 }, the_interval);
