099819e671a4e8bcc5faac3f118bf017aa94f85f
Baargav Venkatesh <kon7341@ex.kroger.com>
2014-09-01 15:23:02 -0400

    Extracted Css Js into seperate files

---
+++ public/javascripts/maincontroller.js
@@ -0,0 +1,18 @@
+ function upVote(path){
+    $.ajax({
+  type: 'POST',
+  url: 'http://mighty-woodland-8571.herokuapp.com/vote',
+  data: { "path": path},
+  dataType: 'json',
+  success: function(data){
+    var voteSpanId = path+"-vote";
+    var voteCountSpan = document.getElementById(voteSpanId);
+    var vote = voteCountSpan.innerHTML;
+    vote++;
+    voteCountSpan.innerHTML = vote;
+  },
+  error: function(xhr, type){
+    alert('Oops! We couldn\'t register your vote');
+  }
+});
+    }
