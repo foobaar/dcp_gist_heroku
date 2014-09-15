78dbfb8c72109854568671811f3670fbff42cc8f
Baargav Venkatesh <kon7341@ex.kroger.com>
2014-09-14 22:34:02 -0400

    Bug Squashed..almost. Maybe.

--- public/javascripts/maincontroller.js
+++ public/javascripts/maincontroller.js
@@ -2,7 +2,7 @@
     $.ajax({
   type: 'POST',
   url: 'http://mighty-woodland-8571.herokuapp.com/vote',
-  data: { "path": gist.path},
+  data: { "path": path},
   dataType: 'json',
   success: function(data){
     var voteSpanId = path+"-vote";
